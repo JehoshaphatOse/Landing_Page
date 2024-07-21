0aa989cf74877003e802cb6a973f4a298e2bfb4f
JehoshaphatOse <jehoshaphatomon@gmail.com>
2024-07-21 19:37:08 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+        bold: 700,
+      },
+      screens: {
+        tablet: "873px",
+        mobile: "580px",
+      },
+      colors: {
+        primary: "#FFB84C",
+        secondary: "#2B2730",
+        tertiary: "#F6F6F6",
+      },
+      backgroundImage: {
+        bgImage: "url(../src/assets/images/banner.png)",
+      },
+      boxShadow: {
+        bottom: ".1px 7px 0px 1px #FFB84C",
+      },
+      keyframes: {
+        beep: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.04)" },
+        },
+      },
+      animation: {
+        beep: "beep 2s infinite",
+      },
+    },
+  },
+  plugins: [],
+};
